9af50e68f18cc7933245fffd2c55cdb132588c3c
Alexander-Berndsen-Peano-5Bi <56531563+Alexander-Berndsen-Peano-5Bi@users.noreply.github.com>
2019-10-14 08:56:11 +0200

    Add files via upload
    Sostituito Scanner con uno Stream

--- ServerTcp.java
+++ ServerTcp.java
@@ -5,7 +5,9 @@
  */
 package servertcp;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
@@ -25,10 +27,11 @@ public class ServerTcp {
         ServerSocket welcomSocket = new ServerSocket(3333);
         for (;;) {
             Socket connectionSocket = welcomSocket.accept();
-
+            
             // Usate gli Stream e non lo Scanner
-            Scanner in = new Scanner(connectionSocket.getInputStream());
-            message = in.nextLine();
+            //Scanner in = new Scanner(connectionSocket.getInputStream());
+            BufferedReader in = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            message = in.readLine();
 
             System.out.println("il server ha ricevuto dal nodo: " + connectionSocket.getRemoteSocketAddress() + " che ci scrive: " + message);
 
